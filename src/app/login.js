926dcfbdfcbd32bd609d0566a2b0583ea08d57e5
LeonardoMeyer <leonardo.meyerr@outlook.com>
2024-12-05 17:24:34 -0300

    logo

--- src/app/login.js
+++ src/app/login.js
@@ -1,75 +1,87 @@
-import { ScrollView, StyleSheet, View, Text, TextInput, Alert } from 'react-native'
-import Button from '../Views/components/Button'
-import { useRouter } from 'expo-router'
-import {useState} from 'react'
-import { useLoginStore } from '../stores/useLoginStore'
-import { storeObjectData } from '../utils/asyncStorage'
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, View, Text, TextInput, Alert, Image } from 'react-native';
+import Button from '../Views/components/Button';
+import { useRouter } from 'expo-router';
+import { useLoginStore } from '../stores/useLoginStore';
+import { storeObjectData } from '../utils/asyncStorage';
+import { SvgUri } from 'react-native-svg';
 
 export default function Login() {
-  const router = useRouter()
-
-  const {login: loginStore} = useLoginStore()
-
-  const [txtEmail, setTxtEmail] = useState('')
-  const [txtPass, setTxtPass] = useState('')
+  const router = useRouter();
+  const { login: loginStore } = useLoginStore();
+  
+  const [txtEmail, setTxtEmail] = useState('');
+  const [txtPass, setTxtPass] = useState('');
 
   const handleLogin = async () => {
     const login = {
-        email: txtEmail,
-        pass: txtPass
-    }
+      email: txtEmail,
+      pass: txtPass,
+    };
 
     const response = await fetch('http://localhost:3000/auth/login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(login)
-    })
+      body: JSON.stringify(login),
+    });
 
-    if(response.ok){
-        const data = await response.json()
-        console.log(data)
-        loginStore({accessToken: data?.accessToken, ...data.user})
-        await storeObjectData('userLogged', {accessToken: data?.accessToken, ...data.user})
-        router.push('/home')
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      loginStore({ accessToken: data?.accessToken, ...data.user });
+      await storeObjectData('userLogged', { accessToken: data?.accessToken, ...data.user });
+      router.push('/home');
     } else {
-        const data = await response.json()
-        Alert.alert('Erro ao logar')
-        console.log(data?.error)
+      const data = await response.json();
+      Alert.alert('Erro ao logar');
+      console.log(data?.error);
     }
-    return
-}
+  };
 
   return (
-      <ScrollView style={styles.container}>
-        <View style={{ flex: 1, marginTop: 100, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Email:</Text>
-          <TextInput 
-            style={styles.input}
-            onChangeText={setTxtEmail}
-            value={txtEmail}
-          />
-          <Text>Senha:</Text>
-          <TextInput 
-            style={styles.input}
-            onChangeText={setTxtPass}
-            value={txtPass}
-            secureTextEntry={true}
-          />
-          <Button onPress={handleLogin}>Entrar</Button>
-        
-          <View style={styles.divisor}/>
+    <ScrollView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require('../../logo/logo.svg')} style={styles.logo} />
+      </View>
 
-          <Button onPress={() => router.push('/signup')}>Cadastre-se</Button>
-        </View>
-      </ScrollView>
-  )
+      <View style={{ flex: 1, marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Email:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setTxtEmail}
+          value={txtEmail}
+        />
+        <Text>Senha:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setTxtPass}
+          value={txtPass}
+          secureTextEntry={true}
+        />
+        <Button onPress={handleLogin}>Entrar</Button>
+
+        <View style={styles.divisor} />
+
+        <Button onPress={() => router.push('/signup')}>Cadastre-se</Button>
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
   },
   input: {
     borderWidth: 1,
@@ -78,12 +90,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 6,
     marginVertical: 5,
-    borderRadius: 5
+    borderRadius: 5,
   },
   divisor: {
     borderBottomColor: '#CCC',
     borderBottomWidth: 1,
     width: '90%',
     marginVertical: 50,
-  }
-})
+  },
+});
